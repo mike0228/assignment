7597fbe6362dc5b37975579953af9315407309c1
mike0228 <mike990228@gmail.com>
2019-09-05 02:13:50 +0800

    fix index

--- src/main/java/com/example/assignment/controller/AuthorizeController.java
+++ src/main/java/com/example/assignment/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.example.assignment.dto.AccessToken;
 import com.example.assignment.dto.Guser;
 import com.example.assignment.support.GithubSupport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +13,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubSupport githubSupport;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.url}")
+    private String redirectUrl;
+
     @GetMapping("/backlocal")
     public  String backlocal(@RequestParam(name="code")String code , @RequestParam(name="state")String state) {
         AccessToken accesstoken =new AccessToken();
-        accesstoken.setClient_id("8f3a6ee67cc571ebf8e6");
+        accesstoken.setClient_id(clientId);
         accesstoken.setCode(code);
-        accesstoken.setRedirect_uri("http://localhost:8080/backlocal");
+        accesstoken.setRedirect_uri(redirectUrl);
         accesstoken.setState(state);
-        accesstoken.setClient_secret("47a396e3fe2532dfd9572090f17522a16bbef55c");
+        accesstoken.setClient_secret(clientSecret);
         String token = githubSupport.getAccessToken(accesstoken);
         Guser user = githubSupport.gtuser(token);
         System.out.println(user.getName());
