3215cb08a854acdf2b433c5caa4da40791c90278
mike0228 <mike990228@gmail.com>
2019-09-06 00:51:49 +0800

    grammar fix

--- src/main/java/com/example/assignment/mapper/QuestionMapper.java
+++ src/main/java/com/example/assignment/mapper/QuestionMapper.java
@@ -7,5 +7,5 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface QuestionMapper {
     @Insert("insert into (title,description,gmt_create,gmt_modified,creator,comment_count,view_count,like_count,tag) values (#{title},#{description},#{gmt_create},#{gmt_modified},#{creator},#{comment_count},#{view_count},#{like_count},#{tag})")
-    void create(Question queston);
+    void create(Question question);
 }
