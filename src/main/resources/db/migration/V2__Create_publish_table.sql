59d8e4d901fe1668eb3a56771b16c502dc6de558
driftwoodu <18851072662@163.com>
2019-09-06 00:34:11 +0800

    th

---
+++ src/main/resources/db/migration/V2__Create_publish_table.sql
@@ -0,0 +1,14 @@
+create table publish
+(
+	id int auto_increment,
+	title varchar(60),
+	description text,
+	gmt_create bigint,
+	gmt_modified bigint,
+	creator int,
+	comment_count int default 0,
+	view_count int default 0,
+	like_count int default 0,
+	tag varchar(256),
+    primary key (id)
+);
