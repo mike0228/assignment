5391de40ab7651b8ce39e876f8383f1877edc181
mrtake233 <18622002148@163.com>
2019-09-06 17:57:48 +0800

    confict fix

--- src/main/java/com/example/assignment/mapper/PostInMapper.java
+++ src/main/java/com/example/assignment/mapper/PostInMapper.java
@@ -9,8 +9,7 @@ import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 @Mapper
-public interface PostInMapper {
-<<<<<<< HEAD
+public interface PostInMapper {gi
     @Insert("insert into postin (title,description,gmt_create,gmt_modified,creator,comment_count,view_count,like_count,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{commentCount},#{viewCount},#{likeCount},#{tag})")
     void create(PostIn postIn);
 
@@ -19,11 +18,4 @@ public interface PostInMapper {
 
     @Select("select count(1) from postin")
     Integer count();
-=======
-    @Insert("insert into postIn (title,description,gmt_create,gmt_modified,creator,comment_count,view_count,like_count,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{commentCount},#{viewCount},#{likeCount},#{tag})")
-    void create(PostIn postIn);
-
-    @Select("select * from postIn")
-    List<PostIn> list();
->>>>>>> 4ab3b5e353868e201a26a5ea0ed74c85144d5d83
 }
